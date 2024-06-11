b5843b2152cd3da30fc655a824017c1d84756647
YuchenLi27 <yuyuchenchen27@gmail.com>
2024-06-10 22:07:10 -0700

    update Dao

--- Dao/db_secrets.py
+++ Dao/db_secrets.py
@@ -7,9 +7,19 @@ from config import DB_SECRET_NAME, AWS_REGION_NAME
 
 def get_secret():
     session = boto3.session.Session()
-    client = session.client(service_name = 'secretsmanager',
-                            region_name= AWS_REGION_NAME,
+    client = session.client(
+        service_name='secretsmanager',
+        region_name= AWS_REGION_NAME,
     )
 
     try:
-        
+        get_secret_value_response = client.get_secret_value(
+            SecretId=DB_SECRET_NAME
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    secret_dict = json.loads(secret)
+    return secret_dict["username"], secret_dict["password"]
+
